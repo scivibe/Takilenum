d08525cafe31ed5c6f74ccae583d770201c7c149
Star Tammes <stammes@wowway.com>
2026-03-30 18:05:22 -0500

    Added basic explanation of features

--- Takilenum.py
+++ Takilenum.py
@@ -1,9 +1,44 @@
 from Glyph import write_takilenum
 
+'''
+A quick note on the available letters
+First off, there is no c. It either gets absorbed into the ch character, or is replaced with a k
+There are special characters for ng, sh, and zh
+There are also special characters for th and dh, if you want to differentiate th is the th sound as in "thistle" and dh is the th sound as in "this"
+Vowels are a, e, i, o, u, and y
+If you enable doLongVowels, the long vowels would be written as aa, ee, ii, oo, uu, and yy
+Also there's no puncuation or other special characters
+Otherwise, here's the list of all letters available. If you use one that hasn't been created, it'll yell at you. 
+t
+d
+p
+b
+k
+g
+q
+l
+r
+m
+n
+ng
+f
+v
+th
+dh
+s
+z
+sh
+zh
+j
+w
+x
+h
+'''
 
 write_takilenum("this is a test of takiilenum script in english mode", outputFile="glyph1.png", maxGlyphPerRow=6, doLongVowels=False)
 write_takilenum("this is a test of takiilenum script in takiilenum mode and also bigger and longer script giving more and more wrapping", outputFile="glyph2.png", maxGlyphPerRow=4, glyphSize=500, doLongVowels=True)
 write_takilenum("and this here is one last sentence to just show off the rest of the features ive got notice how there is no puncuation or apostrophes or shit like that", outputFile="glyph3.png", maxGlyphPerRow=10, glyphSize=250, doLongVowels=True, writingColor=(1.0, 0.0, 0.0), backgroundColor=(0.0, 0.0, 1.0), padding=10)
+write_takilenum("The quick brown fox jumps over the lazy dog", outputFile="glyph4.png", maxGlyphPerRow=6, doLongVowels=False)
 
 
 #Optional code to immediately display results instead of having to dig through folder
@@ -23,6 +58,11 @@ plt.imshow(img)
 plt.show()
 
 img = np.asarray(Image.open("glyph3.png"))
+plt.figure()
+plt.imshow(img)
+plt.show()
+
+img = np.asarray(Image.open("glyph4.png"))
 plt.figure()
 plt.imshow(img)
 plt.show()
